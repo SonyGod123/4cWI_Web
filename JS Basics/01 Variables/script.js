9b1c9d705dab756dd4ee77cb37572c6225c1548d
SonyGod123 <jan.tschabrun@hotmail.com>
2022-10-21 09:29:28 +0200

    Einführung & 01 Variablen

---
+++ JS Basics/01 Variables/script.js
@@ -0,0 +1,30 @@
+console.log("Hallo");
+console.log("Tolle Sache");
+// Variablen
+let a = 7;
+let b = 8;
+let c = 10;
+// Berechne folgendes und gib das Eregebnis über console.log aus
+// a + b + c
+console.log("result: " + a + b + c);
+// (a + b) / c
+console.log("result: " + (a + b) / c);
+// c -a + b
+console.log("result: " + (c - a + b));
+// a + b + c * 3
+console.log("result: " + a + b + c * 3);
+// (a + b + c)/4
+console.log("result: " + (a + b + c) / 4);
+
+// Ändere die Variablen auf folgende Werte
+// a = 3
+a = 3;
+// b = a + 3
+b = a + 3;
+// c = c -3
+c = c + 3;
+// gib die neuen Werte der Variablen aus
+console.log("a: " + a);
+console.log("b: " + b);
+console.log("c: " + c);
+
---
+++ JS Basics/Einführung/script.js
@@ -0,0 +1,20 @@
+console.log("Hallo");
+console.log("Tolle Sache");
+
+//Variablen
+let a = 7;
+let b = 8;
+let c = a + b
+
+console.log("A hat den Wert:" + " " + a);
+
+a = 10;
+console.log("A hat den Wert:" + " " + a);
+
+console.log("a + b = " + c);
+
+// Rechnungsarten
+// *, /, +, -
+
+c = b - a;
+console.log("b - a = " + c);
