4ee74893328a21fb932694c9525a3539e8b1dc53
SonyGod123 <jan.tschabrun@hotmail.com>
2022-11-15 12:09:36 +0100

    JS Strings und Zahlen

---
+++ JS Basics/02 JS Strings_und_Zahlen/script.js
@@ -0,0 +1,33 @@
+let a = 7;
+let b = 8;
+
+let ab = a + b;
+
+//+, -, *, //
+
+let c = 7.4;
+let d = 7.5;
+
+result = c + d;
+console.log(result);
+
+// Test STRINGS
+
+let name2 = "Andreas";
+console.log(name2);
+
+console.log("Das Ergebnis von " + c + " und " + d + " ist " + result);
+
+
+let e = 17.4;
+let f = 3.9;
+
+result2 = e / f;
+console.log(result2);
+
+console.log("The result of " + e + " divided by " + f + " is: " + result2);
+
+let firstname = "Jan";
+let lastname = "Tschabrun";
+
+console.log("Ich heiße " + firstname + " " + lastname);
