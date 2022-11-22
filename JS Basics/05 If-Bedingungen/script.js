e8c37fa28ee47a750fb8cd04474d92de811ba0a4
SonyGod123 <jan.tschabrun@hotmail.com>
2022-11-22 12:30:16 +0100

    04 Schleifen & 05 If-Bedingung

---
+++ JS Basics/05 If-Bedingungen/script.js
@@ -0,0 +1,31 @@
+// Aufgabe 1
+
+let number = 51
+// Wenn die Zahl kleiner ist als 20  gib aus "Mini"
+if (number<20) {
+    console.log("Mini");
+}
+// Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
+if (number >= 20 && number<=50) {
+    console.log("Medium");
+}
+// Wenn die Zahl größer als 50 ist gib aus "Large"
+if (number>50) {
+    console.log("Large");
+}
+
+
+// Aufgabe 2
+let number2 = 91;
+// Wenn die Zahl kleiner gleich 7 ist gib aus "Mini"
+if (number2<=7) {
+    console.log("Mini");
+}
+// Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium"
+if (number2 >= 7 && number2<=90) {
+    console.log("Medium");
+}
+// Wenn die Zahl größer als 90 ist gib aus "Large"
+if (number2>90) {
+    console.log("Large");
+}
