c7cdb02190b5131b48e2a78852430b92d18c60e7
SonyGod123 <jan.tschabrun@hotmail.com>
2022-11-29 12:54:51 +0100

    08 Switch Statement

---
+++ JS Basics/08 Switch Statement/script.js
@@ -0,0 +1,10 @@
+// Erstelle eine Zufallszahl zwischen 5 und 10
+let max = 10;
+let min = 5;
+let randomNumber = (Math.random() * (max-min)) + min;
+let randomasInteger = Math.round(randomNumber);
+console.log(randomasInteger);
+// Wenn der Wert 10 ist gib aus Ten
+// Wenn der Wert 9 ist gib aus Nine
+// Wenn der Wert 8 ist gib aus Eight
+// etc.
