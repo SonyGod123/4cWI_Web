6ac0f916a61a4ac9d095fbd14e0c9cccc77ecf95
SonyGod123 <jan.tschabrun@hotmail.com>
2022-12-16 09:30:18 +0100

    10_Calendar

---
+++ JS Basics/10_Calendar/script.js
@@ -0,0 +1,45 @@
+let month = "January";
+let startday = 3;
+let numDays;
+
+
+switch (month) {
+    case "January":
+        numDays = 31;
+        break;
+    case "February":
+        numDays = 28;
+        break;
+    case "March":
+        numDays = 31;
+        break;
+    case "April":
+        numDays = 30;
+        break;
+    case "May":
+        numDays = 31;
+        break;
+    case "June":
+        numDays = 30;
+        break;
+    case "July":
+        numDays = 31;
+        break;
+    case "August":
+        numDays = 31;
+        break;
+    case "Septembre":
+        numDays = 30;
+        break;
+    case "Octobre":
+        numDays = 31;
+        break;
+    case "Novembre":
+        numDays = 30;
+        break;
+    case "Decembre":
+        numDays = 31;
+        break;
+}
+
+let calendardays = ("| MO | TUE | WE | TH | FR | SA | SU ")
